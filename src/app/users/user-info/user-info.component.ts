9e6ef01203c6a2683d99380a4b3017d7dcb9fcbf
Geass <geass1000@gmail.com>
2017-08-08 16:00:02 +0300

    Add: User info component was added

---
+++ src/app/users/user-info/user-info.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	moduleId : module.id,
+  selector : 'sl-user-info',
+  templateUrl : 'user-info.component.html',
+  styleUrls : [ 'user-info.component.scss' ]
+})
+export class UserInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
