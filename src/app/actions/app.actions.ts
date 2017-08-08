b0ffef46aba3fb8ec4631e5cb70148516a68bdf4
Geass <geass1000@gmail.com>
2017-08-08 17:06:32 +0300

    Add: Action to set users data is added

--- src/app/actions/app.actions.ts
+++ src/app/actions/app.actions.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { IAction } from '../shared/interfaces/action.interface';
+import { IRUsers } from '../shared/interfaces/users.interface';
 
 @Injectable()
 export class AppActions {
@@ -8,6 +9,9 @@ export class AppActions {
 	static readonly OPEN_PANEL = AppActions.CLASS_NAME + 'OPEN_PANEL';
 	static readonly CLOSE_ACTIVE_MODAL = AppActions.CLASS_NAME + 'CLOSE_ACTIVE_MODAL';
 
+	static readonly SET_USERS = AppActions.CLASS_NAME + 'SET_USERS';
+
+	/* Modal */
 	openModal (name : string, state : boolean = true) : IAction {
     return {
       type : AppActions.OPEN_MODAL,
@@ -29,6 +33,16 @@ export class AppActions {
 	closeActiveModal () : IAction {
     return {
       type : AppActions.CLOSE_ACTIVE_MODAL
+    };
+  }
+
+	/* Users Module */
+	setUsers (users : IRUsers) : IAction {
+    return {
+      type : AppActions.SET_USERS,
+			payload : {
+				users : users
+			}
     };
   }
 }
