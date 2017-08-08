c5ee317b93a786c9870300a826969186972e2f19
Angular CLI <angular-cli@angular.io>
2017-08-08 12:53:45 +0300

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { SlDevtestPage } from './app.po';
+
+describe('sl-devtest App', () => {
+  let page: SlDevtestPage;
+
+  beforeEach(() => {
+    page = new SlDevtestPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
