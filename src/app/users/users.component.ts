af32ecb19df97e722d024421f8284b0010150a99
Geass <geass1000@gmail.com>
2017-08-08 17:05:28 +0300

    Add: method to get users data is added and one was called in OnInit event

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+/* App Redux and Request */
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import { NgRedux } from '@angular-redux/store';
+import { AppActions } from '../actions/app.actions';
+
+/* App Services */
+import { LoggerService } from '../core/logger.service';
+import { UsersService } from './users.service';
+
+/* App Interfaces and Classes */
+import { IRUsers } from '../shared/interfaces/users.interface';
 
 @Component({
 	moduleId : module.id,
@@ -6,11 +19,33 @@ import { Component, OnInit } from '@angular/core';
 	templateUrl : 'users.component.html',
 	styleUrls : [ 'users.component.scss' ]
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+	/* Subscriptions */
+	private subscription : Array<Subscription> = [];
 
-  ngOnInit() {
+  constructor(private ngRedux : NgRedux<any>,
+							private appActions : AppActions,
+							private logger : LoggerService,
+						 	private usersService : UsersService) { }
+
+  ngOnInit () {
+		this.getUserList();
   }
+	ngOnDestroy () {
+		this.subscription.map((data) => data.unsubscribe());
+  }
+
+	getUserList () {
+		let sub : Subscription = null;
+		sub = this.usersService.getUserList().subscribe(
+			(data : IRUsers) => {
+				if (data) {
+					this.ngRedux.dispatch(this.appActions.setUsers(data));
+				}
+			}
+		);
+		this.subscription.push(sub);
+	}
 
 }
