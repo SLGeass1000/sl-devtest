64085c8a6f2a9da5c06ec39ac1d06226765623a0
Geass <geass1000@gmail.com>
2017-08-08 14:16:52 +0300

    ADD: Core and Shared modules and Not Found component was added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
+import { NgReduxModule } from '@angular-redux/store';
+
+/* Feature Modules */
+import { CoreModule } from './core/core.module';
+
+/* Routing Module */
+import { AppRoutingModule } from './app-routing.module';
+
+/* App Root */
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+/* App Root - Actions */
+import { AppActions } from './actions/app.actions';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+		BrowserModule,
+		BrowserAnimationsModule,
+		CoreModule,
+		FormsModule,
+		ReactiveFormsModule,
+		NgReduxModule,
+		/* Feature Modules */
+		/* Main App Router */
+		AppRoutingModule
+	],
+  declarations: [
+		AppComponent,
+		NotFoundComponent
+	],
+	providers: [
+		AppActions
+	],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
