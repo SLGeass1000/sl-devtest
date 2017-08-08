9ffa66e4a7e6d201dd8b8700d07aea77f0539250
Geass <geass1000@gmail.com>
2017-08-09 02:30:37 +0300

    Fix: Code was fixed for compliancing the TSLint rules

--- src/app/users/users.component.spec.ts
+++ src/app/users/users.component.spec.ts
@@ -3,8 +3,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { UsersComponent } from './users.component';
 
 describe('UsersComponent', () => {
-  let component: UsersComponent;
-  let fixture: ComponentFixture<UsersComponent>;
+  let component : UsersComponent;
+  let fixture : ComponentFixture<UsersComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
