d1fac1b5a1f5c7f10e61286a95dfab9c5c119616
Geass <geass1000@gmail.com>
2017-08-08 17:07:02 +0300

    Add: State for users data is added

--- src/app/reducers/state.reducer.ts
+++ src/app/reducers/state.reducer.ts
@@ -2,15 +2,23 @@ import { Reducer } from 'redux';
 import { IAction } from '../shared/interfaces/action.interface';
 import { AppActions } from '../actions/app.actions';
 
+import { IRUsers } from '../shared/interfaces/users.interface';
 
 export interface IState {
+	users : IRUsers;
 }
 
 export const INITIAL_STATE : IState = {
+	users : null
 };
 
 export const StateReducer : Reducer<IState> = (state = INITIAL_STATE, action : IAction) : IState => {
 	switch (action.type) {
+		case AppActions.SET_USERS : {
+			return Object.assign({}, state, {
+				users : action.payload.users
+			});
+		}
 	}
 	return state;
 };
