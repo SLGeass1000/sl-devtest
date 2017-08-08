a6450485a87b9f9f33043d6a3e4ce21fa136ec2b
Geass <geass1000@gmail.com>
2017-08-08 14:10:26 +0300

    Chore: Component for Not Found page was added

---
+++ src/app/not-found/not-found.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	moduleId: module.id,
+	selector: 'sl-not-found',
+	templateUrl: 'not-found.component.html',
+  styleUrls: [ 'not-found.component.scss' ]
+})
+export class NotFoundComponent {}
