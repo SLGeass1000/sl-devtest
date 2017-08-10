22b3e66c3ad427db25cb90321fb92379075f6fc4
Geass <geass1000@gmail.com>
2017-08-10 17:43:07 +0300

    Add: Base class for controllers was added

---
+++ server/lib/base-controller.class.ts
@@ -0,0 +1,45 @@
+import { logger } from '../config/logger.config';
+import * as express from 'express';
+
+export class BaseController {
+	public logger : any;
+
+	constructor () {
+		this.logger = logger;
+	}
+
+	/**
+	 * sendSuccessResponse - send success response.
+	 *
+	 * @method
+	 *
+	 * @param {express.Response} resp - express response object
+	 * @param {number} statusCode - status code
+	 * @param {Object} data - any payload
+	 * @param {string} methodName - sender method name
+	 * @param {string} message - logging message
+	 * @return {void}
+	 */
+	sendSuccessResponse (resp : express.Response, statusCode : number,
+											 data : any, methodName : string, message : string) : void {
+		this.logger.info(`${this.constructor.name} - ${methodName}:`, `${statusCode} -`, message);
+		resp.status(statusCode).json(data);
+	}
+
+	/**
+	 * sendErrorResponse - send error response.
+	 *
+	 * @method
+	 *
+	 * @param {express.Response} resp - express response object
+	 * @param {Object} error - any Error object
+	 * @param {string} methodName - sender method name
+	 * @return {void}
+	 */
+	sendErrorResponse (resp : express.Response, error : any,
+										 methodName : string, message : string) : void {
+		const statusCode = error.statusCode ? error.statusCode : 500;
+		this.logger.error(`${this.constructor.name} - ${methodName}:`, `${statusCode} -`, message);
+		resp.status(statusCode).json({ 'error' : message });
+	}
+}
