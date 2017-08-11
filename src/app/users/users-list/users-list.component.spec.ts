f6bc087848eb1a45413215bc950fb56ca7494d25
Geass <geass1000@gmail.com>
2017-08-11 16:36:52 +0300

    Add: Configure TestModule was added

--- src/app/users/users-list/users-list.component.spec.ts
+++ src/app/users/users-list/users-list.component.spec.ts
@@ -1,6 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgReduxTestingModule, MockNgRedux } from '@angular-redux/store/testing';
 
 import { UsersListComponent } from './users-list.component';
+import { Router, RouterStub } from '../../../testing/router.stub';
+import { NgRedux, NgReduxStub } from '../../../testing/redux.stub';
+import { LoggerService, LoggerServiceStub } from '../../../testing/logger.stub';
+
+import { AppActions } from '../../actions/app.actions';
+class AppActionsStub {
+}
 
 describe('UsersListComponent', () => {
   let component : UsersListComponent;
@@ -8,7 +16,13 @@ describe('UsersListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UsersListComponent ]
+      declarations: [ UsersListComponent ],
+			providers: [
+      { provide: Router, useClass: RouterStub },
+			{ provide: NgRedux, useClass: NgReduxTestingModule },
+			{ provide: AppActions, useClass: AppActionsStub },
+			{ provide: LoggerService, useClass: LoggerServiceStub }
+    ]
     })
     .compileComponents();
   }));
