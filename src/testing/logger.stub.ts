01999921d76ebb7ad5196cc30455861ee2d1ecff
Geass <geass1000@gmail.com>
2017-08-11 16:34:12 +0300

    Add: Stub LoggerService was added

---
+++ src/testing/logger.stub.ts
@@ -0,0 +1,10 @@
+export { LoggerService } from '../app/core/logger.service';
+
+export class LoggerServiceStub {
+	constructor () { ; }
+	info () { ; }
+	log () { ; }
+	warn () { ; }
+	error () { ; }
+	debug () { ; }
+}
