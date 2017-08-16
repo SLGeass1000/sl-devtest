8121d2b46a17ed2abe3439c41b744fca4076e5e4
Geass <geass1000@gmail.com>
2017-08-16 05:00:00 +0300

    Chore: Code was cleanned

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit, OnDestroy, ElementRef, Renderer2 } from '@angular/core';
-import { Router } from '@angular/router';
 
 /* App Redux and Request */
 import { AppReducer, INITIAL_STATE, IApp } from './reducers/app.store';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 import { NgRedux, select } from '@angular-redux/store';
-import { AppActions } from './actions/app.actions';
 
 /* App Services */
 import { LoggerService } from './core/logger.service';
@@ -20,23 +18,19 @@ import { LoggerService } from './core/logger.service';
 	styleUrls : [ 'app.component.scss' ]
 })
 export class AppComponent implements OnInit, OnDestroy {
-	private animationState : string = 'open';
 	public scrollHidden : boolean = false;
 
 	/* Redux */
 	private subscription : Array<Subscription> = [];
 	@select(['modal', 'openModalOverlay']) openModalOverlay$ : Observable<boolean>;
 	@select(['modal', 'open']) modalOpen$ : Observable<boolean>;
-	@select(['state', 'sid']) sid$ : Observable<string>;
 
 	constructor(private elementRef: ElementRef,
 							private renderer: Renderer2,
-							private router : Router,
 							private ngRedux : NgRedux<IApp>,
-							private appActions : AppActions,
 						 	private logger : LoggerService) {
 		this.ngRedux.configureStore(AppReducer, INITIAL_STATE, null, []);
-		this.logger.info(`${this.constructor.name}:`, 'Start app Artificial System!');
+		this.logger.info(`${this.constructor.name}:`, 'Start app!');
 	}
 	ngOnInit () {
 		let sub : Subscription;
